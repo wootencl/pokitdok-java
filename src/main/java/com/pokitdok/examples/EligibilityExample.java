f3c9a27e6f9986a36c561d6b72548d00cb40dbeb
John Riney <john.riney@pokitdok.com>
2014-07-18 23:01:24 -0400

    example code

--- src/main/java/com/pokitdok/examples/EligibilityExample.java
+++ src/main/java/com/pokitdok/examples/EligibilityExample.java
@@ -1,11 +1,35 @@
 package com.pokitdok.examples;
 
+import java.util.Map;
 import com.pokitdok.PokitDok;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
 public class EligibilityExample {
   public static void main(String argv[]) {
     try {
       PokitDok pd = new PokitDok("2MBlqahR2xiaBtVSS50n", "FJaN1fyB1V5q7qPLNrb2F6yV1Xkaui0OB6eXotOS");
+
+      StringBuffer eligibilityRequest = new StringBuffer();
+      eligibilityRequest.append("{");
+      eligibilityRequest.append("\"member\": {");
+      eligibilityRequest.append("\"birth_date\": \"1970-01-01\",");
+      eligibilityRequest.append("\"first_name\": \"Jane\",");
+      eligibilityRequest.append("\"last_name\": \"Doe\",");
+      eligibilityRequest.append("\"id\": \"W000000000\"");
+      eligibilityRequest.append("},");
+      eligibilityRequest.append("\"provider\": {");
+      eligibilityRequest.append("\"first_name\": \"JEROME\",");
+      eligibilityRequest.append("\"last_name\": \"AYA-AY\",");
+      eligibilityRequest.append("\"npi\": \"1467560003\"");
+      eligibilityRequest.append("},");
+      eligibilityRequest.append("\"service_types\": [\"health_benefit_plan_coverage\"],");
+      eligibilityRequest.append("\"trading_partner_id\": \"MOCKPAYER\"");
+      eligibilityRequest.append("}");
+
+      Map eligibilityQuery = (JSONObject) JSONValue.parse(eligibilityRequest.toString());
+      Map<String, Object> response = pd.eligibility(eligibilityQuery);
+      System.out.println(response);
     }
     catch (Exception e) {
       System.out.println("An exception occurred: " + e.toString());
