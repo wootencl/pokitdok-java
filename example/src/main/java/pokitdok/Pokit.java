5a976d87c40fc75e787bad077acc62be8e94621c
Kurt Kraus <kkraus@Kurts-MacBook-Pro.local>
2015-10-03 19:19:00 -0700

    initial add for helloworld

---
+++ example/src/main/java/pokitdok/Pokit.java
@@ -0,0 +1,13 @@
+package pokitdok;
+
+/**
+ * PokitDok!
+ *
+ */
+public class Pokit
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Domination will come for you!!!" );
+    }
+}
